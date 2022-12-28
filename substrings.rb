524d0022452780f70d8197ca657389d6f505f5a3
valdife <salemon876@gmail.com>
2022-12-28 15:05:05 +0100

    refactor substrings to use scan method

--- substrings.rb
+++ substrings.rb
@@ -1,16 +1,11 @@
-def substrings(word, dictionary)
-  # dictionary = dictionary.map { |key| key.to_sym } <- better to use symbols as keys, but Odin asked to not
-  dict_hash = Hash.new(0)
-
-  # regexp selects every char that is NOT (^) caseinsensitive (/i) alphanumerical and includes spaces (0-9a-z )
-  words_array = word.gsub(/[^0-9a-z ]/i, '').split.map { |word| word.downcase }
-  
-  words_array.each do |string|
-    dictionary.each do |key|
-      dict_hash[key] += 1 if string.include? key #.to_s
-    end
+def substrings(text, dictionary)
+  result_hash = Hash.new(0)
+  text.downcase!
+  dictionary.each do |key|
+    matches = text.scan(key).length
+    result_hash[key] = matches if matches != 0 
   end
-  dict_hash
+  result_hash
 end
   
 

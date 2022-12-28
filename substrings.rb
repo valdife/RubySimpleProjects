037400289ac464465bffc533fa59aff74ec50592
valdife <salemon876@gmail.com>
2022-12-28 14:57:40 +0100

    add working substrings project

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,18 @@
 def substrings(word, dictionary)
-    words_array = word.gsub(/[^0-9a-z ]/i, '').split
-    
-    words_array
-end
+  # dictionary = dictionary.map { |key| key.to_sym } <- better to use symbols as keys, but Odin asked to not
+  dict_hash = Hash.new(0)
 
-dictionary = {}
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+  # regexp selects every char that is NOT (^) caseinsensitive (/i) alphanumerical and includes spaces (0-9a-z )
+  words_array = word.gsub(/[^0-9a-z ]/i, '').split.map { |word| word.downcase }
+  
+  words_array.each do |string|
+    dictionary.each do |key|
+      dict_hash[key] += 1 if string.include? key #.to_s
+    end
+  end
+  dict_hash
+end
+  
+
+keys = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("Howdy partner, sit down! How's it going?", keys)
